8120b39154aaccbff10258653209c6d5ddb05df0
ughrima <agrimajain223@gmail.com>
2024-02-20 11:51:45 +0530

    fixed header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,3 @@
-// import React from "react";
-// import "./header.css"
-// import CTA from "./CTA"; 
-// import HeaderSocials from "./headersocials";
-
-// const Header = () => {
-//   return (
-//       <header>
-//       <div className="container header_container">
-//         <h2>Hello I'm</h2>
-//         <h1>Agrima Jain</h1>
-//         <h2>Student Developer</h2>
-//         <CTA/>
-//         <HeaderSocials/>
-//       </div>
-//     </header>
-//   )
-// }
-
-// export default Header
-
 import React, { useEffect, useRef } from 'react';
 import Typed from 'typed.js';
 import "./header.css";
@@ -48,7 +27,7 @@ const Header = () => {
       <div className="container header_container">
         <h2>Hello I'm</h2>
         <h1>Agrima Jain</h1>
-        <h2 ref={typedTextRef}></h2>
+        <p ref={typedTextRef} style={{fontSize:"30px"}}></p>
         <CTA />
         <HeaderSocials />
       </div>
